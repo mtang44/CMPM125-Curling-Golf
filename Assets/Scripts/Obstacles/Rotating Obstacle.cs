38d51ca252f905ccf37956c8f2ebda218053aea2
adegan1 <adegan@ucsc.edu>
2026-06-08 00:23:03 -0700

    Added a simple rotating obstacle

--- Assets/Scripts/Obstacles/Rotating Obstacle.cs
+++ Assets/Scripts/Obstacles/Rotating Obstacle.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class RotatingObstacle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Rotation")]
+    [SerializeField] private Vector3 rotationAxis = Vector3.up;
+    [SerializeField] private float degreesPerSecond = 90f;
+    [SerializeField] private Space rotationSpace = Space.Self;
+
+    private void OnValidate()
     {
-        
+        if (rotationAxis == Vector3.zero)
+        {
+            rotationAxis = Vector3.up;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 axis = rotationAxis.normalized;
+        if (axis.sqrMagnitude < 0.0001f)
+        {
+            return;
+        }
+
+        float frameRotation = degreesPerSecond * Time.deltaTime;
+        transform.Rotate(axis, frameRotation, rotationSpace);
     }
 }
